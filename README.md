cc819560ec787761379419428313ea799355ba96
Leonardo Henrique <leonardo.henrique@lhtecnologia.com>
2023-10-22 11:26:11 -0300

    working

---
+++ README.md
@@ -0,0 +1,3 @@
+# References
+
+[C-SharpCorner - Build MultiRepo MicroFrontEnd Using Angular and Webpack 5 Module Federation](https://www.c-sharpcorner.com/blogs/build-multirepo-microfrontend-using-angular-and-webpack-5-module-fed)
---
+++ bookstore-host/projects/bookstore-shell/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent,
+  pathMatch: 'full'
+}, {
+  path: 'auth',
+  loadChildren: () => import('auth-mfe/Module').then(m => m.AuthModule)
+}, {
+  path: 'books',
+  loadChildren: () => import('book-mfe/Module').then(m => m.BookModule)
+}];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
